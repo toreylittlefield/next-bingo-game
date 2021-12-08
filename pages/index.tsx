a1ee5bafbbebc2ee19bfc7a121f091e4578d85f3
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-08 19:45:20 +0800

    try to connect to pusher but failing? try prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,47 @@ import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 import { useEffect, useState } from 'react';
 import Pusher from 'pusher-js';
+import { pusherClientOptions } from '../lib/pusherClient';
+import { channelName } from '../lib/pusherChannel';
 
-const pusherConfig = {
-  cluster: process.env.PUSHER_CLUSTER as string,
-};
+const { appId, cluster } = pusherClientOptions;
 
 const Home: NextPage = () => {
-  const [state, setstate] = useState();
+  const [userName, setUserName] = useState('');
+  const [submit, setSubmit] = useState(false);
 
   useEffect(() => {
     // Enable pusher logging - don't include this in production
     if (process.env.NODE_ENV === 'development') {
       Pusher.logToConsole = true;
     }
-    console.log(process.env.NEXT_PUBLIC_PUSHER_APP_ID);
-    // const pusher = new Pusher(process.env.PUSHER_APP_ID as string, pusherConfig);
 
-    // const channel = pusher.subscribe('bingo-prod');
-    // channel.bind('message-event', function (data: any) {
-    //   alert(JSON.stringify(data));
-    // });
+    const pusher = new Pusher(appId, {
+      cluster: cluster,
+      // authEndpoint: 'api/pusher/auth',
+      // auth: { params: { userName: 'Torey' } },
+    });
+    const channel = pusher.subscribe(channelName);
+
+    channel.bind('message-event', function (data: any) {
+      alert(JSON.stringify(data));
+    });
   }, []);
 
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <label htmlFor="user-name">Enter username</label>
+      <input
+        id="user-name"
+        placeholder="Enter a username"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      ></input>
+      <button onClick={() => setSubmit(true)} type="button" disabled={submit}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default Home;
