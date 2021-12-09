e4bbaeeaf5fc169030b0fb4f4ea335e04bc543f6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-10 01:02:00 +0800

    update authconext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { AuthContextProvider } from '../stores/useNetlifyIdentity';
+import { AuthContextProvider } from '../stores/netlifyIdentityContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
