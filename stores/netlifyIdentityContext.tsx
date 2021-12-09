1dd52e26089fb376507be3d9b93604575418d6eb
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-09 22:46:29 +0800

    create a context provider to wrap our application for netlify identity widget

---
+++ stores/netlifyIdentityContext.tsx
@@ -0,0 +1,65 @@
+import React, { createContext, useState, useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
+
+interface AuthInterface {
+  user: netlifyIdentity.User | null;
+  login: () => void;
+  logout: () => void;
+  authReady: boolean;
+}
+
+const AuthContext = createContext<AuthInterface | null>({
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false,
+});
+
+interface Props {
+  children: React.ReactNode;
+}
+
+type UserType = netlifyIdentity.User | null;
+
+export const AuthContextProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<UserType>(null);
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    netlifyIdentity.on('login', (user) => {
+      setUser(user);
+      netlifyIdentity.close();
+      console.log('login event');
+    });
+    netlifyIdentity.on('logout', () => {
+      setUser(null);
+      console.log('logout event');
+    });
+    netlifyIdentity.on('init', (user) => {
+      setUser(user);
+      setAuthReady(true);
+      console.log('init event');
+    });
+
+    // init netlify identity connection
+    netlifyIdentity.init();
+
+    return () => {
+      netlifyIdentity.off('login');
+    };
+  }, []);
+
+  const login = () => {
+    netlifyIdentity.open();
+  };
+
+  const logout = () => {
+    netlifyIdentity.logout();
+  };
+
+  const context = { user, login, logout, authReady };
+
+  return <AuthContext.Provider value={context}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
