cbf94dd4d1e629ecb0c6ffabb8ef4f47f08110dd
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-11 20:27:54 +0800

    finish nav with login and logout buttons

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,10 @@
-import { Button, Flex, Heading, Stack } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Stack } from '@chakra-ui/react';
 import Link from 'next/link';
 import Image from 'next/image';
 import React, { useContext } from 'react';
 import AuthContext from '../stores/netlifyIdentityContext';
 
-import { AiOutlineMail as EmailIcon } from 'react-icons/ai';
+import { AiOutlineLogin as LoginIcon, AiOutlineLogout as Logout } from 'react-icons/ai';
 const Nav = () => {
   const { user, login, logout, authReady } = useContext(AuthContext);
 
@@ -16,47 +16,32 @@ const Nav = () => {
       wrap="wrap"
       padding="1.5rem"
       bgGradient="linear(to-l,#313084, #831F3B)"
-      color="whit(to-r, greee"
     >
       <Flex align="center" mr={5} gap={5}>
-        <Image
-          src="https://res.cloudinary.com/boar-images/image/upload/v1639182352/boar-coders-assets/boarcoders-tshirt_oshejd.png"
-          alt="boar coders logo"
-          width={80}
-          height={80}
-        />
+        <Box>
+          <Image
+            src="https://res.cloudinary.com/boar-images/image/upload/v1639182352/boar-coders-assets/boarcoders-tshirt_oshejd.png"
+            alt="boar coders logo"
+            width={80}
+            height={80}
+          />
+        </Box>
         <Heading as="h1" size="lg" letterSpacing={'-.1rem'} color="whiteAlpha.900">
           Boar Coders Bingo
         </Heading>
       </Flex>
       <Stack direction="row" spacing={4}>
-        <Button leftIcon={<EmailIcon />} colorScheme="teal" variant="solid">
-          Email
-        </Button>
-        <Button rightIcon={<EmailIcon />} colorScheme="teal" variant="outline">
-          Call us
-        </Button>
+        {authReady && !user && (
+          <Button onClick={login} leftIcon={<LoginIcon />} colorScheme="teal" variant="solid">
+            Login
+          </Button>
+        )}
+        {user && (
+          <Button onClick={logout} rightIcon={<Logout />} color="red.400" colorScheme="cyan" variant="outline">
+            Logout
+          </Button>
+        )}
       </Stack>
-      {authReady && (
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          {!user && (
-            <li onClick={login} className="btn">
-              Login/Signup
-            </li>
-          )}
-          {user && <li>{user.email}</li>}
-          {user && (
-            <li onClick={logout} className="btn">
-              Logout
-            </li>
-          )}
-        </ul>
-      )}
     </Flex>
   );
 };
