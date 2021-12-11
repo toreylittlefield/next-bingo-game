095dcf607b1142bbf100c827892b19a12954c3a5
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-11 14:44:19 +0800

    add chakra provider and cssreset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthContextProvider } from '../stores/netlifyIdentityContext';
+import Nav from '../components/Nav';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthContextProvider>
-      <Component {...pageProps} />
-    </AuthContextProvider>
+    <ChakraProvider>
+      <CSSReset />
+      <AuthContextProvider>
+        <Nav />
+        <Component {...pageProps} />
+      </AuthContextProvider>
+    </ChakraProvider>
   );
 }
 
