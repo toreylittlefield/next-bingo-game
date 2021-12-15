f822201d7ac506b77e6fec3cf4e37881adb36564
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-15 17:17:46 +0800

    update settings button to go to userprofile

--- components/SettingsButton.tsx
+++ components/SettingsButton.tsx
@@ -2,6 +2,7 @@ import { Tooltip, Button, Text, useMediaQuery } from '@chakra-ui/react';
 import { AiOutlineSetting as SettingIcon } from 'react-icons/ai';
 import React from 'react';
 import netlifyIdentity from 'netlify-identity-widget';
+import NextLink from 'next/link';
 
 type PropTypeUserSettings = {
   user: netlifyIdentity.User;
@@ -11,11 +12,13 @@ const SettingsButton = ({ user }: PropTypeUserSettings) => {
   const [screen480above] = useMediaQuery('(min-width: 480px)');
 
   return (
-    <Tooltip hasArrow label="Change User Settings" bg="blue.600">
+    <NextLink href={'/userprofile'} passHref>
       <Button rightIcon={<SettingIcon />} color="ghostwhite" variant="link">
-        <Text>{screen480above ? user.email : 'Settings'}</Text>
+        <Tooltip hasArrow label="Change User Settings" bg="blue.600">
+          <Text>{screen480above ? user.email : 'Settings'}</Text>
+        </Tooltip>
       </Button>
-    </Tooltip>
+    </NextLink>
   );
 };
 
