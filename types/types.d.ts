5583eb83630843577bcec73c3ef0609cc6849309
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-12-22 22:16:09 +0800

    fix types for goTrue

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,15 +1,12 @@
-import netlifyIdentity from 'netlify-identity-widget';
+import * as netlifyIdentity from 'netlify-identity-widget';
 import GoTrue from './gotrue';
 
 export type UserNameStates = 'loading' | 'sent' | 'ready' | 'sub';
 
 export type UserProfile = netlifyIdentity.User;
 
-export type GoTrueProps = {
-  gotrue: GoTrue;
-};
-export type NetlifyGoTrueIdentity = GoTrueProps & Omit<typeof netlifyIdentity, 'netlifyGoTrue'>;
-
 declare module 'netlify-identity-widget' {
-  export const netlifyGoTrue: NetlifyGoTrueIdentity;
+  declare let gotrue: GoTrue;
+
+  export { gotrue };
 }
