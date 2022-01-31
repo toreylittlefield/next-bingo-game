1499d60a934753f4a08310ba7f220ed71158322d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-31 08:55:25 +0800

    redirect on login

--- functions/identity-login.ts
+++ functions/identity-login.ts
@@ -1,16 +1,21 @@
 import { Handler } from '@netlify/functions';
 
 const handler: Handler = async (event, context) => {
-  console.log({ context });
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({ message: JSON.stringify(context.clientContext) }),
-  // };
+  if (context.clientContext) {
+    const { clientContext } = context;
+    console.log(JSON.stringify(clientContext.user_metadata, null, 2));
+    return {
+      statusCode: 302,
+      headers: {
+        Location: '/userprofile',
+        'Cache-Control': 'no-cache',
+      },
+      body: '',
+    };
+  }
   return {
-    statusCode: 302,
-    headers: {
-      Location: '/userprofile',
-    },
+    statusCode: 403,
+    body: JSON.stringify({ message: 'Not Authorized' }),
   };
 };
 
