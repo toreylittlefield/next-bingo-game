2e12aaa410f07001767ef9348f09a4578502ad1f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-31 10:06:27 +0800

    log payload & context

--- functions/identity-login.ts
+++ functions/identity-login.ts
@@ -1,9 +1,11 @@
 import { Handler } from '@netlify/functions';
 
 const handler: Handler = async (event, context) => {
-  if (context.clientContext) {
+  if (context.clientContext && event.body) {
     const { clientContext } = context;
-    console.log(JSON.stringify(clientContext, null, 2));
+    const payload = JSON.parse(event.body);
+    const user = payload.user;
+    console.log(JSON.stringify({ clientContext, user, payload }, null, 2));
     return {
       statusCode: 302,
       headers: {
