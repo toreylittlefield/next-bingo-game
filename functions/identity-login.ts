1d58a0041ff2a8eb9709a5981377c4f4f65125ba
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-31 21:04:18 +0800

    identity-login create user in fauna db

--- functions/identity-login.ts
+++ functions/identity-login.ts
@@ -1,18 +1,64 @@
 import { Handler } from '@netlify/functions';
+import faunadb, { Collection, Create } from 'faunadb';
+import generator from 'generate-password';
+import * as netlifyIdentity from 'netlify-identity-widget';
+
+type Key = {
+  secret: string;
+};
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_ADMIN_KEY as string,
+});
+
+/* create a user in FaunaDB that can connect from the browser */
+function createUser(userData: netlifyIdentity.User, password: string) {
+  return client.query(
+    q.Create(q.Class('users'), {
+      credentials: {
+        password: password,
+      },
+      data: {
+        id: userData.id,
+        user_metadata: userData.user_metadata,
+      },
+    })
+  );
+}
+
+function obtainToken(user: netlifyIdentity.User, password: string) {
+  return client.query(q.Login(q.Select('ref', user), { password }));
+}
 
 const handler: Handler = async (event, context) => {
   if (context.clientContext && event.body) {
     const { clientContext } = context;
     const payload = JSON.parse(event.body);
-    const user = payload.user;
-    console.log(JSON.stringify({ clientContext, user, payload }, null, 2));
+    const userData = payload.user;
+    console.log(JSON.stringify({ clientContext, userData, payload }, null, 2));
+    const password = generator.generate({
+      length: 10,
+      numbers: true,
+    });
+
+    const user = await createUser(userData, password);
+    const key = (await obtainToken(user as netlifyIdentity.User, password)) as Key;
+
     return {
       statusCode: 302,
       headers: {
         Location: '/userprofile',
         'Cache-Control': 'no-cache',
       },
-      body: '',
+      body: JSON.stringify({
+        app_metadata: {
+          faunadb_token: key.secret,
+          // we discard the credential, and can create a new one if we ever need a new token
+          // faunadb_credential : password
+        },
+      }),
     };
   }
   return {
