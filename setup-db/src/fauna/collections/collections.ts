41ae59f954471ee8698c10311ec1fc639c8f2ff7
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-17 17:18:41 +0800

    update collections with index boards by ref

--- setup-db/src/fauna/collections/collections.ts
+++ setup-db/src/fauna/collections/collections.ts
@@ -1,4 +1,6 @@
 import fauna from 'faunadb';
+import { CreateIndexAllBingoBoards } from '../indexes/faunaIndex.js';
+import { indexAllBingoBoardsByRef } from '../indexes/faunaIndexesNames.js';
 import { bingoBoardCollections, usersCollection } from './collectionnames.js';
 const q = fauna.query;
 
@@ -10,7 +12,7 @@ const CreateBingoBoardsCollection = CreateCollection(bingoBoardCollections);
 async function createCollections(faunaClient: fauna.Client) {
   await faunaClient.query(If(Exists(Collection(usersCollection.name)), true, CreateUsersCollection));
   await faunaClient.query(If(Exists(Collection(bingoBoardCollections.name)), true, { CreateBingoBoardsCollection }));
-  //   await faunaClient.query(If(Exists(Index('all_fweets')), true, CreateIndexAllFweets));
+  await faunaClient.query(If(Exists(Index(indexAllBingoBoardsByRef.name)), true, CreateIndexAllBingoBoards));
   //   await faunaClient.query(If(Exists(Index('fweets_by_author')), true, CreateIndexFweetsByAuthor));
   //   await faunaClient.query(If(Exists(Index('fweets_by_tag')), true, CreateIndexFweetsByTag));
   //   await faunaClient.query(If(Exists(Index('fweets_by_reference')), true, CreateIndexFweetsByReference));
