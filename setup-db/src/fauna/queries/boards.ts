13af4b4e942b2fd01bf1a2714abc2fac66b247cf
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-19 23:10:04 +0800

    TODO functions to create user, account, and manipulate boards

--- setup-db/src/fauna/queries/boards.ts
+++ setup-db/src/fauna/queries/boards.ts
@@ -1,15 +1,58 @@
 import faunadb from 'faunadb';
-import { bingoBoardCollections } from '../collections/collectionnames.js';
+import { bingoBoardCollections, usersCollection } from '../collections/collectionnames.js';
 const q = faunadb.query;
-const { Var, Now, Select, Get, CurrentIdentity, Let, Create, Collection } = q;
+const { Var, Ref, Now, Select, Get, Delete, CurrentIdentity, Let, Create, Collection, Update, Paginate, Documents } = q;
 
+export function createAccount() {}
+
+/** USER FUNCTIONS */
+export function createUser(name: faunadb.Expr, alias: faunadb.Expr, icon: faunadb.Expr): faunadb.Expr {
+  const FQLStatement = Let(
+    {
+      // accountRef: CurrentIdentity(),
+      // userRef: Select(['data', 'user'], Get(Var('accountRef'))),
+      createUser: Create(Collection(usersCollection.name), {
+        data: {
+          name: name,
+          alias: alias,
+          icon: icon,
+        },
+      }),
+    },
+    Var('createUser')
+  );
+  return FQLStatement;
+}
+
+export function UpdateUser(name: faunadb.Expr, alias: faunadb.Expr, icon: faunadb.Expr): faunadb.Expr {
+  const FQLStatement = Let(
+    {
+      accountRef: CurrentIdentity(),
+      userRef: Select(['data', 'user'], Get(Var('accountRef'))),
+      updateUser: Update(Var('userRef'), {
+        data: {
+          name: name,
+          alias: alias,
+          icon: icon,
+        },
+      }),
+    },
+    Var('updateUser')
+  );
+  return FQLStatement;
+}
+/** -----> END USER FUNCTIONS */
+
+/** Boards Functions */
 export function createBoard(title: faunadb.ExprArg, board: faunadb.ExprArg) {
   const FQLStatement = Let(
     {
+      accountRef: CurrentIdentity(),
+      userRef: Select(['data', 'user'], Get(Var('accountRef'))),
       newBoard: Create(Collection(bingoBoardCollections.name), {
         data: {
           title: title,
-          author: Select(['data', 'id'], Get(CurrentIdentity())),
+          author: Var('userRef'),
           board: board,
           // we will order by creation time, we already have 'ts' by default but updated will also update 'ts'.
           created: Now(),
@@ -17,6 +60,51 @@ export function createBoard(title: faunadb.ExprArg, board: faunadb.ExprArg) {
       }),
     },
     Var('newBoard')
+  );
+
+  return FQLStatement;
+}
+
+/** update board */
+export function updateBoard(title: faunadb.ExprArg, board: faunadb.ExprArg) {
+  const FQLStatement = Let(
+    {
+      accountRef: CurrentIdentity(),
+      userRef: Select(['data', 'user'], Get(Var('accountRef'))),
+      newBoard: Update(Collection(bingoBoardCollections.name), {
+        data: {
+          title: title,
+          author: Var('userRef'),
+          board: board,
+          // we will order by creation time, we already have 'ts' by default but updated will also update 'ts'.
+        },
+      }),
+    },
+    Var('newBoard')
+  );
+
+  return FQLStatement;
+}
+
+/** read board */
+export function readAllBoards(title: faunadb.ExprArg) {
+  const FQLStatement = Let(
+    {
+      AllBoards: Paginate(Documents(Collection(bingoBoardCollections.name))),
+    },
+    Var('AllBoards')
+  );
+
+  return FQLStatement;
+}
+
+/** delete board */
+export function deleteBoard(ref: faunadb.ExprArg) {
+  const FQLStatement = Let(
+    {
+      DeleteBoard: Delete(Ref(Collection(bingoBoardCollections.name), ref)),
+    },
+    Var('DeleteBoard')
   );
 
   return FQLStatement;
