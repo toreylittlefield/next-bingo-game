e64e3410c6d4d0a3d9fefbda363a1dc1877fca67
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-19 23:08:49 +0800

    add accounts & users collections

--- setup-db/src/fauna/collections/collectionnames.ts
+++ setup-db/src/fauna/collections/collectionnames.ts
@@ -1,6 +1,11 @@
+export const accountsCollection = {
+  name: 'accounts',
+};
+
 export const usersCollection = {
   name: 'users',
 };
+
 export const bingoBoardCollections = {
   name: 'bingo_boards',
 };
