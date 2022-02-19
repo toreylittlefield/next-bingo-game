0b6e65b6f51f6fa7b13c3408a82d96e1833f26be
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-19 23:09:11 +0800

    create users & accounts collections with indexes

--- setup-db/src/fauna/collections/collections.ts
+++ setup-db/src/fauna/collections/collections.ts
@@ -1,22 +1,25 @@
 import fauna from 'faunadb';
-import { CreateIndexAllBingoBoards } from '../indexes/faunaIndex.js';
-import { indexAllBingoBoardsByRef } from '../indexes/faunaIndexesNames.js';
-import { bingoBoardCollections, usersCollection } from './collectionnames.js';
+import { CreateIndexAllBingoBoards, CreateIndexByAccountId } from '../indexes/faunaIndex.js';
+import { indexAllAccountsById, indexAllBingoBoardsByRef } from '../indexes/faunaIndexesNames.js';
+import { accountsCollection, bingoBoardCollections, usersCollection } from './collectionnames.js';
 const q = fauna.query;
 
 const { CreateCollection, Collection, Exists, If, Index } = q;
 
+/** Accounts Collection */
+const CreateAccountsCollection = CreateCollection(accountsCollection);
+/** Users Collection */
 const CreateUsersCollection = CreateCollection(usersCollection);
+/** Boards Collection */
 const CreateBingoBoardsCollection = CreateCollection(bingoBoardCollections);
 
+/** Create All Collections & Indexes Here  */
 async function createCollections(faunaClient: fauna.Client) {
+  await faunaClient.query(If(Exists(Collection(accountsCollection.name)), true, CreateAccountsCollection));
   await faunaClient.query(If(Exists(Collection(usersCollection.name)), true, CreateUsersCollection));
-  await faunaClient.query(If(Exists(Collection(bingoBoardCollections.name)), true, { CreateBingoBoardsCollection }));
+  await faunaClient.query(If(Exists(Collection(bingoBoardCollections.name)), true, CreateBingoBoardsCollection));
+  await faunaClient.query(If(Exists(Index(indexAllAccountsById.name)), true, CreateIndexByAccountId));
   await faunaClient.query(If(Exists(Index(indexAllBingoBoardsByRef.name)), true, CreateIndexAllBingoBoards));
-  //   await faunaClient.query(If(Exists(Index('fweets_by_author')), true, CreateIndexFweetsByAuthor));
-  //   await faunaClient.query(If(Exists(Index('fweets_by_tag')), true, CreateIndexFweetsByTag));
-  //   await faunaClient.query(If(Exists(Index('fweets_by_reference')), true, CreateIndexFweetsByReference));
-  //   await faunaClient.query(If(Exists(Index('fweets_by_hashtag_ref')), true, CreateIndexFweetsByHashtagRef));
 }
 
 export { createCollections };
