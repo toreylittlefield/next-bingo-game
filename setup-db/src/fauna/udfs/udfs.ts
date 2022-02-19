09363de2217a27aac76e0bf556a187c5dbb8e88c
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-19 23:11:35 +0800

    create UDFs for users & boards

--- setup-db/src/fauna/udfs/udfs.ts
+++ setup-db/src/fauna/udfs/udfs.ts
@@ -1,7 +1,14 @@
 import faunadb from 'faunadb';
-import { createBoard } from '../queries/boards.js';
-import { functionsBingoBoardsRole } from '../roles/rolenames.js';
-import { createBoardUDFname } from './udfnames.js';
+import { createBoard, createUser, deleteBoard, readAllBoards, updateBoard, UpdateUser } from '../queries/boards.js';
+import { functionsBingoBoardsRole, serverAccountRole } from '../roles/rolenames.js';
+import {
+  createBoardUDFname,
+  createUserUDFname,
+  deleteBoardUDFname,
+  readBoardUDFname,
+  updateBoardUDFname,
+  updateUserUDFname,
+} from './udfnames.js';
 const q = faunadb.query;
 const { Var, Query, Lambda, Exists, If, Update, Select, Get, CreateFunction, Role, CurrentIdentity } = q;
 
@@ -20,8 +27,43 @@ function CreateOrUpdateFunction(obj: UDF) {
   );
 }
 
+/** USER MANIPULATION */
+export const createUserUDF = CreateFunction({
+  name: createUserUDFname.name,
+  body: Query(Lambda(['name', 'alias', 'icon'], createUser(Var('name'), Var('alias'), Var('icon')))),
+  role: serverAccountRole.name,
+  // only the server can create the account & user
+});
+
+export const updateUserUDF = CreateFunction({
+  name: updateUserUDFname.name,
+  body: Query(Lambda(['name', 'alias', 'icon'], UpdateUser(Var('name'), Var('alias'), Var('icon')))),
+  role: Role(functionsBingoBoardsRole.name),
+});
+
+/** -----> END USER MANIPULATION */
+
+/** BOARDS MANIPULATION */
 export const createBoardUDF = CreateOrUpdateFunction({
   name: createBoardUDFname.name,
   body: Query(Lambda(['title', 'board'], createBoard(Var('title'), Var('board')))),
+  role: Role(functionsBingoBoardsRole.name),
+});
+
+/** Paginates all boards from the collection */
+export const readBoardUDF = CreateOrUpdateFunction({
+  name: readBoardUDFname.name,
+  body: Query(Lambda(['ref'], readAllBoards(Var('title')))),
+  role: Role(functionsBingoBoardsRole.name),
+});
+
+export const updateBoardUDF = CreateOrUpdateFunction({
+  name: updateBoardUDFname.name,
+  body: Query(Lambda(['title', 'board'], updateBoard(Var('title'), Var('board')))),
+  role: Role(functionsBingoBoardsRole.name),
+});
+export const deleteBoardUDF = CreateOrUpdateFunction({
+  name: deleteBoardUDFname.name,
+  body: Query(Lambda(['ref'], deleteBoard(Var('ref')))),
   role: Role(functionsBingoBoardsRole.name),
 });
