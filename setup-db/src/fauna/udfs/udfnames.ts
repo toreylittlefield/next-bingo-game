37b1d4d86d27c29b95890e712ce06b92428aa308
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-19 23:10:58 +0800

    TODO udf function names create, read, update, delete

--- setup-db/src/fauna/udfs/udfnames.ts
+++ setup-db/src/fauna/udfs/udfnames.ts
@@ -1,3 +1,27 @@
+export const createAccountUDFname = {
+  name: 'create_account',
+};
+
+export const createUserUDFname = {
+  name: 'create_user',
+};
+
+export const updateUserUDFname = {
+  name: 'update_user',
+};
+
 export const createBoardUDFname = {
   name: 'create_board',
+};
+
+export const readBoardUDFname = {
+  name: 'read_board',
+};
+
+export const updateBoardUDFname = {
+  name: 'update_board',
+};
+
+export const deleteBoardUDFname = {
+  name: 'delete_board',
 };
