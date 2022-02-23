b1adf46aab3c7a82fcf15bf21d1976c059233b62
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-23 22:32:23 +0800

    update the new indexes for token refresh

--- setup-db/src/fauna/collections/collections.ts
+++ setup-db/src/fauna/collections/collections.ts
@@ -1,6 +1,16 @@
 import fauna from 'faunadb';
-import { CreateIndexAllBingoBoards, CreateIndexByAccountId } from '../indexes/faunaIndex.js';
-import { indexAllAccountsById, indexAllBingoBoardsByRef } from '../indexes/faunaIndexesNames.js';
+import {
+  CreateIndexAccessTokenByRefreshTokenRef,
+  CreateIndexAllBingoBoards,
+  CreateIndexByAccountId,
+  CreateIndexTokensByInstance,
+} from '../indexes/faunaIndex.js';
+import {
+  indexAccessTokenByRefreshToken,
+  indexAllAccountsById,
+  indexAllBingoBoardsByRef,
+  indexTokensByInstance,
+} from '../indexes/faunaIndexesNames.js';
 import { accountsCollection, bingoBoardCollections, usersCollection } from './collectionnames.js';
 const q = fauna.query;
 
@@ -15,11 +25,18 @@ const CreateBingoBoardsCollection = CreateCollection(bingoBoardCollections);
 
 /** Create All Collections & Indexes Here  */
 async function createCollections(faunaClient: fauna.Client) {
+  /** collections */
   await faunaClient.query(If(Exists(Collection(accountsCollection.name)), true, CreateAccountsCollection));
   await faunaClient.query(If(Exists(Collection(usersCollection.name)), true, CreateUsersCollection));
   await faunaClient.query(If(Exists(Collection(bingoBoardCollections.name)), true, CreateBingoBoardsCollection));
+
+  /** indexes */
   await faunaClient.query(If(Exists(Index(indexAllAccountsById.name)), true, CreateIndexByAccountId));
   await faunaClient.query(If(Exists(Index(indexAllBingoBoardsByRef.name)), true, CreateIndexAllBingoBoards));
+  await faunaClient.query(
+    If(Exists(Index(indexAccessTokenByRefreshToken.name)), true, CreateIndexAccessTokenByRefreshTokenRef)
+  );
+  await faunaClient.query(If(Exists(Index(indexTokensByInstance.name)), true, CreateIndexTokensByInstance));
 }
 
 export { createCollections };
