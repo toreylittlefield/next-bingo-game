d37e9d99fd73c576c2ee1b46ff609088f7d79161
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-23 22:31:56 +0800

    udfs for register, login, logout, and refresh token

--- setup-db/src/fauna/udfs/udfs.ts
+++ setup-db/src/fauna/udfs/udfs.ts
@@ -1,16 +1,42 @@
 import faunadb from 'faunadb';
+import { accountsCollection } from '../collections/collectionnames.js';
+import { LoginAccount } from '../functions/login.js';
+import {
+  ACCESS_TOKEN_LIFETIME_SECONDS,
+  CreateAccessAndRefreshToken,
+  REFRESH_TOKEN_LIFETIME_SECONDS,
+} from '../functions/tokens.js';
 import { createBoard, createUser, deleteBoard, readAllBoards, updateBoard, UpdateUser } from '../queries/boards.js';
 import { functionsBingoBoardsRole, serverAccountRole } from '../roles/rolenames.js';
 import {
   createBoardUDFname,
   createUserUDFname,
   deleteBoardUDFname,
+  loginAccountUDFname,
+  logoutAndDeleteTokensAccountUDFname,
   readBoardUDFname,
+  refreshTokenUDFname,
+  registerAccountUDFname,
   updateBoardUDFname,
   updateUserUDFname,
 } from './udfnames.js';
 const q = faunadb.query;
-const { Var, Query, Lambda, Exists, If, Update, Select, Get, CreateFunction, Role, CurrentIdentity } = q;
+const {
+  Create,
+  Collection,
+  Var,
+  Query,
+  Lambda,
+  Exists,
+  If,
+  Update,
+  Logout,
+  Select,
+  Get,
+  CreateFunction,
+  Role,
+  CurrentIdentity,
+} = q;
 
 // A convenience function to either create or update a function.
 type UDF = {
@@ -27,15 +53,66 @@ function CreateOrUpdateFunction(obj: UDF) {
   );
 }
 
+/** REFRESH TOKEN */
+export const createRefreshTokenUDF = CreateOrUpdateFunction({
+  name: refreshTokenUDFname.name,
+  body: Query(
+    Lambda([], {
+      tokens: CreateAccessAndRefreshToken(
+        CurrentIdentity(),
+        ACCESS_TOKEN_LIFETIME_SECONDS,
+        REFRESH_TOKEN_LIFETIME_SECONDS
+      ),
+      account: Get(CurrentIdentity()),
+    })
+  ),
+  role: serverAccountRole.name,
+  // only the server can create the account & user
+});
+
+/** CREATE | REGISTER ACCOUNT */
+export const registerCreateAccountUDF = CreateOrUpdateFunction({
+  name: registerAccountUDFname.name,
+  body: Query(
+    Lambda(
+      ['id', 'password'],
+      Create(Collection(accountsCollection.name), {
+        // credentials is a special field, the contents will never be returned
+        // and will be encrypted. { password: ... } is the only format it currently accepts.
+        credentials: { password: Var('password') },
+        // everything you want to store in the document should be scoped under 'data'
+        data: {
+          id: Var('id'),
+        },
+      })
+    )
+  ),
+  role: serverAccountRole.name,
+});
+
+/** LOGIN ACCOUNT & CREATE USER */
+export const loginAccountAndCreateUserUDF = CreateOrUpdateFunction({
+  name: loginAccountUDFname.name,
+  body: Query(Lambda(['id', 'password'], LoginAccount(Var('id'), Var('password')))),
+  role: serverAccountRole.name,
+});
+
+//** LOGOUT ACCOUNT & DELETE TOKENS */
+export const logoutDeleteTokensAccountUDF = CreateOrUpdateFunction({
+  name: logoutAndDeleteTokensAccountUDFname.name,
+  body: Query(Lambda(['all'], Logout(Var('all')))),
+  role: serverAccountRole.name,
+});
+
 /** USER MANIPULATION */
-export const createUserUDF = CreateFunction({
+export const createUserUDF = CreateOrUpdateFunction({
   name: createUserUDFname.name,
   body: Query(Lambda(['name', 'alias', 'icon'], createUser(Var('name'), Var('alias'), Var('icon')))),
   role: serverAccountRole.name,
   // only the server can create the account & user
 });
 
-export const updateUserUDF = CreateFunction({
+export const updateUserUDF = CreateOrUpdateFunction({
   name: updateUserUDFname.name,
   body: Query(Lambda(['name', 'alias', 'icon'], UpdateUser(Var('name'), Var('alias'), Var('icon')))),
   role: Role(functionsBingoBoardsRole.name),
