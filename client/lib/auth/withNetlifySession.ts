fcaded3a30c4dab2f3ea3b0345077eebc0249fc3
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-04 20:41:43 +0800

    getServerSideProps to check Netlify session

---
+++ client/lib/auth/withNetlifySession.ts
@@ -0,0 +1,57 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { NetlifyAppMetaData } from '../../types/types';
+import { NETLIFY_SITE_URL } from '../constants/constants';
+
+export function withNetlifySession<P extends { [key: string]: unknown } = { [key: string]: unknown }>(
+  innerHandler: ({
+    context,
+    user,
+  }: {
+    context?: GetServerSidePropsContext;
+    user?: NetlifyAppMetaData;
+  }) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>,
+  options?: any,
+) {
+  return async function getSSPWrappedNetlifySession(context: GetServerSidePropsContext) {
+    const token = context.req?.cookies?.[`nf-jwt`];
+    try {
+      var user;
+      const userRes = await fetch(`${NETLIFY_SITE_URL}/.netlify/identity/user`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      user = await userRes.json();
+    } catch (error) {
+      console.error(error);
+    }
+
+    return innerHandler({ context, user });
+  };
+}
+
+export function returnUserOrLogin<P extends { [key: string]: unknown } = { [key: string]: unknown }>(
+  user: NetlifyAppMetaData | undefined,
+  callback?: (key?: P) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>,
+) {
+  if (callback) {
+    return callback();
+  }
+  if (!user) {
+    return {
+      redirect: {
+        destination: `/login`,
+        permanent: true,
+      },
+    };
+  }
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+export const getNetlifyUserSession = withNetlifySession(async function getIdentitySession({ context, user }) {
+  return returnUserOrLogin(user);
+});
