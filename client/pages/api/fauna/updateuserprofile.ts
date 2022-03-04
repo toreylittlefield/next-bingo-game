88a2bd65bf143823a5bd3bc16024b66e7181e8db
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-04 20:53:54 +0800

    ts ignore

--- client/pages/api/fauna/updateuserprofile.ts
+++ client/pages/api/fauna/updateuserprofile.ts
@@ -32,6 +32,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     //     context: { clientContext },
     //   },
     // } = req;
+    //@ts-ignore
     return res.status(200).json({ message: JSON.stringify(req.netlifyFunctionParams) });
   } catch (error) {
     console.log('error', error);
