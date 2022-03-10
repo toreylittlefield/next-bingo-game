214e6c2a0618940a2c9df44fbeac2f1752273e93
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-10 17:16:43 +0800

    refactor types

--- client/stores/netlifyIdentityContext.tsx
+++ client/stores/netlifyIdentityContext.tsx
@@ -1,9 +1,10 @@
 import React, { createContext, useState, useEffect } from 'react';
 import netlifyIdentity from 'netlify-identity-widget';
 import Router from 'next/router';
+import type { NetlifyAppMetaData } from '../types/types';
 
 interface AuthInterface {
-  user: netlifyIdentity.User | null;
+  user: NetlifyAppMetaData | null;
   login: () => void;
   logout: () => void;
   authReady: boolean;
@@ -29,21 +30,22 @@ interface UserType extends netlifyIdentity.User {
 }
 
 export const AuthContextProvider = ({ children }: Props) => {
-  const [user, setUser] = useState<UserType | null>(null);
+  const [user, setUser] = useState<NetlifyAppMetaData | null>(null);
   const [authReady, setAuthReady] = useState(false);
 
   useEffect(() => {
-    netlifyIdentity.on('login', (user: UserType) => {
-      console.log({ user }, 'user login');
+    /** fires when users logs in or visits page or has gotrue in local storage */
+    netlifyIdentity.on('login', (u) => {
+      console.log({ u }, 'user login');
+      const user = u as NetlifyAppMetaData;
       setUser(user);
       netlifyIdentity.close();
-      if (!user.user_metadata.username?.trim()) {
-        Router.push('/userprofile');
-        return;
-      } else {
-        Router.push('/rooms');
-      }
-      console.log('login event');
+      // if (!user.user_metadata.username?.trim()) {
+      //   Router.push('/userprofile');
+      //   return;
+      // } else {
+      //   Router.push('/rooms');
+      // }
     });
     netlifyIdentity.on('logout', () => {
       setUser(null);
@@ -51,19 +53,10 @@ export const AuthContextProvider = ({ children }: Props) => {
       console.log('logout event');
     });
 
-    netlifyIdentity.on('init', async (user) => {
-      console.log({ user }, 'init user');
-      // if (user?.token?.access_token) {
-      //   await netlifyIdentity.gotrue.currentUser()?.update({
-      //     data: {
-      //       test_update: false,
-      //     },
-      //   });
-      //   await netlifyIdentity.refresh(true);
-      //   const currentUser = netlifyIdentity.currentUser();
-      //   setUser(currentUser);
-      //   return;
-      // }
+    //** fires when netlify is first initialize */
+    netlifyIdentity.on('init', async (u) => {
+      console.log({ user: u }, 'init user');
+      const user = u as NetlifyAppMetaData;
       setUser(user);
       setAuthReady(true);
       console.log('init event');
