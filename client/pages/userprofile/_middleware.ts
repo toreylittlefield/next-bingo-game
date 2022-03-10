3ca0897c5ba321401f3c17872906c92ff3b1a031
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-10 20:11:52 +0800

    fix: absolute urls only in redirects

--- client/pages/userprofile/_middleware.ts
+++ client/pages/userprofile/_middleware.ts
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextMiddleware, NextResponse } from 'next/server';
 import { getIdentitySession } from '../../lib/auth/getIdentitySession';
+import { NETLIFY_SITE_URL } from '../../lib/constants/constants';
 
 const authIdentityMiddleware: NextMiddleware = async (req, event) => {
   const { user, setLocalTestCookie } = await getIdentitySession(req);
 
-  if (!user) return NextResponse.redirect('/login');
+  if (!user) return NextResponse.redirect(`${NETLIFY_SITE_URL}/login`);
 
   const res = NextResponse.next();
   if (setLocalTestCookie?.token) {
