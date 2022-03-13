6a3df85bf6dcf57ba75297743e9ca8cb2bf6ec2f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-14 05:54:34 +0800

    refactor: return if no access token

--- client/lib/auth/verifyIndentity.ts
+++ client/lib/auth/verifyIndentity.ts
@@ -2,6 +2,10 @@ import type { NetlifyAppMetaData } from '../../types/types';
 import { NETLIFY_SITE_URL } from '../constants/constants';
 
 export async function verifyIdentity(access_token: string) {
+  if (!access_token) {
+    console.error('no access token', { access_token });
+    return;
+  }
   try {
     const userRes = await fetch(`${NETLIFY_SITE_URL}/.netlify/identity/user`, {
       headers: {
