ce4cf162df72c211e26d6b19f626631beeb023f8
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-14 19:44:49 +0800

    update: route to userprofile/me

--- client/components/SettingsButton.tsx
+++ client/components/SettingsButton.tsx
@@ -8,7 +8,13 @@ const SettingsButton = ({ user }: UserProfile) => {
   const [screen480above] = useMediaQuery('(min-width: 480px)');
 
   return (
-    <NextLink href={`userprofile/me?userprofile=${user.user_metadata.full_name}`} passHref>
+    <NextLink
+      href={{
+        pathname: 'userprofile/me/[userprofile]',
+        query: { userprofile: user.user_metadata.full_name },
+      }}
+      passHref
+    >
       <Button rightIcon={<SettingIcon />} color="ghostwhite" variant="link">
         <Tooltip hasArrow label="Change User Settings" bg="blue.600">
           <Text>{screen480above ? user.email : 'Settings'}</Text>
