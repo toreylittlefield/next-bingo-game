1feb92e8a23a065612df33c6a9d10b5f3f14b1b2
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-20 10:46:25 +0800

    temp: comment out useless crap

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 import React, { useEffect, useRef, useState } from 'react';
 import Pusher from 'pusher-js';
 import { channelName } from '../lib/pusherChannel';
-import { Container, Flex } from '@chakra-ui/react';
+import { Container, Flex, Text } from '@chakra-ui/react';
 import { UserNameStates } from '../types/types';
 import SendMessageForm from '../components/SendMessageForm';
 
@@ -13,31 +13,33 @@ interface Data {
 }
 
 const Home: NextPage = () => {
-  const [submit, setSubmit] = useState<UserNameStates>('ready');
-  const [liveChatMessages, setLiveChatMessages] = useState<Data[]>([]);
-  const pusherRef = useRef<Pusher>(null);
+  // const [submit, setSubmit] = useState<UserNameStates>('ready');
+  // const [liveChatMessages, setLiveChatMessages] = useState<Data[]>([]);
+  // const pusherRef = useRef<Pusher>(null);
 
-  useEffect(() => {
-    if (submit !== 'sub' || pusherRef.current == null) return;
-    setSubmit('sent');
-    // Enable pusher logging - don't include this in production
-    let pusher = pusherRef.current;
-    pusher.subscribe(channelName);
+  // useEffect(() => {
+  //   if (submit !== 'sub' || pusherRef.current == null) return;
+  //   setSubmit('sent');
+  //   // Enable pusher logging - don't include this in production
+  //   let pusher = pusherRef.current;
+  //   pusher.subscribe(channelName);
 
-    pusher.bind('message-event', function (data: Data) {
-      const { message, userName } = data;
-      setLiveChatMessages((prev) => [...prev, { message, userName }]);
-    });
+  //   pusher.bind('message-event', function (data: Data) {
+  //     const { message, userName } = data;
+  //     setLiveChatMessages((prev) => [...prev, { message, userName }]);
+  //   });
 
-    return () => {
-      // if(submit )
-      // if (pusherRef.current?.connection.state) pusherRef.current.unsubscribe(channelName);
-    };
-  }, [submit]);
+  //   return () => {
+  //     // if(submit )
+  //     // if (pusherRef.current?.connection.state) pusherRef.current.unsubscribe(channelName);
+  //   };
+  // }, [submit]);
 
   return (
     <Flex direction="column">
-      {liveChatMessages.map((liveMessage) => {
+      <Text>This is the home page</Text>
+      <Text>Put some home page stuff here</Text>
+      {/* {liveChatMessages.map((liveMessage) => {
         const randomString = Math.random().toString(36).slice(2);
 
         const { message, userName } = liveMessage;
@@ -49,7 +51,7 @@ const Home: NextPage = () => {
       })}
       <Container maxW="container.sm">
         <SendMessageForm ref={pusherRef} setSubmit={setSubmit} submit={submit} />
-      </Container>
+      </Container> */}
     </Flex>
   );
 };
