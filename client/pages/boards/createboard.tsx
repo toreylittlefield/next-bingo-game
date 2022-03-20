9a198301e5e42c373270f5f4aa86260bf781713a
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-20 10:47:18 +0800

    feat: create board page

---
+++ client/pages/boards/createboard.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import { BingoBoard } from '../../components/BingoBoard';
+
+const CreateBoard: NextPage = () => {
+  return <BingoBoard />;
+};
+
+export default CreateBoard;
