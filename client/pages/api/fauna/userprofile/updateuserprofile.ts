0954e6b5a529c977c966ea3a1d89ce4121a89e81
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-21 16:55:04 +0800

    feat: add api route for updating fauna user profile
    Co-authored-by: MacAnderson Uche <MacAndersonUche@users.noreply.github.com>

--- client/pages/api/fauna/userprofile/updateuserprofile.ts
+++ client/pages/api/fauna/userprofile/updateuserprofile.ts
@@ -1,82 +1,57 @@
 /* code from functions/todos-create.js */
-import faunadb, { Collection, Create } from 'faunadb'; /* Import faunaDB sdk */
+import faunadb from 'faunadb'; /* Import faunaDB sdk */
 import type { NextApiHandler } from 'next';
-
-/* configure faunaDB Client with our secret */
-const q = faunadb.query;
-const client = new faunadb.Client({
-  domain: 'db.fauna.com',
-  scheme: 'https',
-  secret: process.env.FAUNADB_SERVER_KEY as string,
-});
-
-type Data = {
-  message: string | object;
-};
-
-interface Message {
-  userName: string;
-  message: string;
-}
+import { updateFaunaUserProfile } from '../../../../lib/faunaApi/udfs/updateFaunaUserProfile';
+import { updateUserYupSchema } from '../../../../lib/yup-schemas/yup-schemas';
+import type { FaunaUpdateUserReqBody } from '../../../../types/types';
 
 /* export our lambda function as named "handler" export */
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const {
-      aborted,
-      body,
-      complete,
-      cookies,
-      env,
-      destroyed,
-      headers,
-      httpVersion,
-      httpVersionMajor,
-      httpVersionMinor,
-      socket,
-      query,
-      rawHeaders,
-      rawTrailers,
-      method,
-    } = req;
-    /* parse the string body into a useable JS object */
-    // const { message, userName }: Message = req.body;
-    // console.log(req);
-    // console.log(req.netlifyFunctionParams);
-    // const {
-    //   //@ts-ignore
-    //   netlifyFunctionParams: {
-    //     context: { clientContext },
-    //   },
-    // } = req;
-    console.log(
-      '******************* START UPDATE USER PROFILE *******************',
+    const netlifyIdentityToken = req.headers.authorization;
 
-      JSON.stringify({
-        aborted,
-        body,
-        complete,
-        cookies,
-        env,
-        destroyed,
-        headers,
-        query,
-        method,
-        //@ts-expect-error
-        context: req.context,
-        // rawHeaders: req.rawHeaders,
-        // cookies: req.cookies,
-        //@ts-expect-error
-        netlifyFunctionParams: req.netlifyFunctionParams,
-        // headers: req.headers,
-        // readReq,
-      }),
-      '******************* END UPDATE USER PROFILE *******************',
-    );
-    return res.status(200).json({ message: JSON.stringify(req.rawHeaders) });
+    const payload: FaunaUpdateUserReqBody = req.body;
+
+    const faunaAccessToken = payload.access_token;
+
+    if (req.method !== 'PATCH') return res.status(405).send('Method not allowed, use PATCH');
+
+    if (!netlifyIdentityToken) return res.status(401).send('Not Authorized, No netlify Identity token');
+
+    if (!faunaAccessToken) return res.status(401).send('Not Authorized, No fauna Token');
+
+    /** when the user first registers and hasn't updated their user profile the lastUpdate will be false */
+    if (payload.lastUpdated === false) payload.lastUpdated = '2021/10/13';
+    console.dir(payload);
+
+    const isValid = await updateUserYupSchema.validate(payload);
+
+    if (!isValid) return res.status(418).send('Invalid payload items');
+
+    const faunaAccessClient = new faunadb.Client({
+      domain: 'db.fauna.com',
+      scheme: 'https',
+      secret: payload.access_token,
+    });
+
+    const faunaRes = await updateFaunaUserProfile(faunaAccessClient, payload);
+
+    if (!faunaRes) throw Error('Error Updating User Profile In Fauna');
+
+    if (faunaRes?.result === false) {
+      return res.status(400).json({ message: 'Cannot Update Fauna User Profile', ...faunaRes });
+    }
+
+    const { compareDates, result } = faunaRes;
+    const { alias, icon, lastUpdated, name } = result.data;
+
+    const resultData = { data: { alias, icon, lastUpdated, name } };
+    const resPayload = { compareDates, result: resultData };
+
+    return res.status(200).json(resPayload);
   } catch (error) {
     console.log('error', error);
-    res.status(400).json({ message: JSON.stringify(error) });
+    res.status(500).json({ message: JSON.stringify(error) });
   }
 };
 
