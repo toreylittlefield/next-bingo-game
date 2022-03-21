04491236a00c75a2991d504750ebd3769dadaed6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-21 16:52:23 +0800

    refactor: update / fix types
    Co-authored-by: MacAnderson Uche <MacAndersonUche@users.noreply.github.com>

--- client/lib/faunaApi/udfs/loginFaunaAccount.ts
+++ client/lib/faunaApi/udfs/loginFaunaAccount.ts
@@ -1,12 +1,9 @@
 import faunadb from 'faunadb';
-import { FaunaLoggedInResponse } from '../../../types/types';
+import type { FaunaLoggedInApiResponse, FaunaLoggedInResponse } from '../../../types/types';
 import { serverClient } from '../server';
 const { Call } = faunadb.query;
 
-export async function loginFaunaAccount(
-  userId: string,
-  password: string,
-): Promise<{ faunaTokens: FaunaLoggedInResponse['tokens']; faunaUser: FaunaLoggedInResponse['user'] } | undefined> {
+export async function loginFaunaAccount(userId: string, password: string): Promise<FaunaLoggedInApiResponse> {
   try {
     const { user, tokens } = (await serverClient.query(Call('login', [userId, password]))) as FaunaLoggedInResponse;
     return { faunaUser: user, faunaTokens: tokens };
