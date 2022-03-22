cae30bee0e3a55e6b868811491419cbc8e581b64
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-22 13:20:55 +0800

    feat: api route to create new bingo board in fauna with user access token
    Co-authored-by: MacAnderson Uche <MacAndersonUche@users.noreply.github.com>

---
+++ client/pages/api/fauna/boards/createboard.ts
@@ -0,0 +1,43 @@
+import type { NextApiHandler } from 'next/types';
+import verifyFaunaRefreshToken from '../../../../lib/faunaApi/verifyFauna';
+import { FaunaCreateBoardRequestBody } from '../../../../types/types';
+import faunadb from 'faunadb';
+const { Call } = faunadb.query;
+import { createBoardYupSchema } from '../../../../lib/yup-schemas/yup-schemas';
+
+const handler: NextApiHandler = async (req, res) => {
+  try {
+    const payload: FaunaCreateBoardRequestBody = req.body;
+    const { fn_tkn } = req.cookies;
+
+    if (!payload.access_token) return res.status(401).send('Invalid access token');
+
+    const isValid = await verifyFaunaRefreshToken(fn_tkn);
+
+    if (!isValid) return res.status(401).send('Not Signed / Invalid Token');
+
+    console.log({ payload });
+    const isValidSchema = await createBoardYupSchema.validate(payload);
+    if (!isValidSchema) return res.status(400).send('Not a valid board bitttchhhh');
+
+    // get board from payload
+    // verify board with the yup schema
+    // if valid call fauna db create board function (UDF)
+    // return res
+    const faunaAccessClient = new faunadb.Client({
+      secret: payload.access_token,
+      domain: 'db.fauna.com',
+      scheme: 'https',
+    });
+
+    const faunaRes = await faunaAccessClient.query(Call('create_board', [payload.title, payload.board]));
+
+    if (!faunaRes) return res.status(400).send('Could Not Create Board In Fauna');
+    return res.status(200).json({ ...faunaRes });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+};
+
+export default handler;
