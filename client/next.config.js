4d67e702d2774a61fac3a372e9da3c6b2fbb637f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-24 18:45:05 +0800

    feat: turn off cloudinary loader and use next for now

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    loader: 'cloudinary',
+    // loader: 'cloudinary',
     domains: ['res.cloudinary.com'],
-    path: 'https://res.cloudinary.com/boar-images/image/upload/',
+    // path: 'https://res.cloudinary.com/boar-images/image/upload/',
   },
 };
