6daed2a509c0add58c1562848d7094cd7f2713f7
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-24 18:46:48 +0800

    add: cloudinary upload user image response types

--- client/types/types.d.ts
+++ client/types/types.d.ts
@@ -344,3 +344,27 @@ export interface FaunaCreateBoardRequestBody {
   title: string;
   board: Array<string>;
 }
+
+interface CloudinaryUploadUserImageResponse {
+  asset_id: string;
+  public_id: string;
+  version: number;
+  version_id: string;
+  signature: string;
+  width: number;
+  height: number;
+  format: string;
+  resource_type: string;
+  created_at: string;
+  tags: any[];
+  bytes: number;
+  type: string;
+  etag: string;
+  placeholder: boolean;
+  url: string;
+  secure_url: string;
+  overwritten: boolean;
+  original_filename: string;
+  original_extension: string;
+  api_key: string;
+}
