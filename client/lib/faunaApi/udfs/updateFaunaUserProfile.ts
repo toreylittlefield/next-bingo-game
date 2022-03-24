6672b8c6921e2183fcc74688727933b2e74d1ee0
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-24 13:25:32 +0800

    refactor: refactor to match types for faunaUser

--- client/lib/faunaApi/udfs/updateFaunaUserProfile.ts
+++ client/lib/faunaApi/udfs/updateFaunaUserProfile.ts
@@ -1,16 +1,23 @@
 import faunadb from 'faunadb';
-import type { UpdateFaunaUserResponse, FaunaUpdateUserApiResponse } from '../../../types/types';
+import type { FaunaNewUserApiResponse, UpdateFaunaSuccessRes } from '../../../types/types';
 const { Call } = faunadb.query;
 
 export async function updateFaunaUserProfile(
   faunaAccessClient: faunadb.Client,
   { name, alias, icon }: { name: string; alias: string; icon: string },
-): Promise<FaunaUpdateUserApiResponse> {
+): Promise<FaunaNewUserApiResponse> {
   try {
     const { compareDates, result } = (await faunaAccessClient.query(
       Call('update_user', [name, alias, icon]),
-    )) as UpdateFaunaUserResponse;
-    return { result, compareDates };
+    )) as UpdateFaunaSuccessRes;
+    if (result === false) {
+      return { result, compareDates };
+    }
+    if (result && result.data.lastUpdated !== false) {
+      const setDate = result.data.lastUpdated['@date'];
+      const data = { ...result.data, lastUpdated: setDate };
+      return { compareDates, result: { data } };
+    }
   } catch (error) {
     console.error(error);
     return undefined;
