b9583058a0dc7a9e929e626d698db973ec05cae9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-25 11:30:58 +0800

    refactor: normalize user lastupdated type

--- client/lib/faunaApi/udfs/loginFaunaAccount.ts
+++ client/lib/faunaApi/udfs/loginFaunaAccount.ts
@@ -6,7 +6,14 @@ const { Call } = faunadb.query;
 export async function loginFaunaAccount(userId: string, password: string): Promise<FaunaLoggedInApiResponse> {
   try {
     const { user, tokens } = (await serverClient.query(Call('login', [userId, password]))) as FaunaLoggedInResponse;
-    return { faunaUser: user, faunaTokens: tokens };
+    if (user.lastUpdated !== false) {
+      const [lastUpdated] = Object.values(user.lastUpdated);
+      const copyUser = { ...user, lastUpdated };
+      return { faunaUser: copyUser, faunaTokens: tokens };
+    }
+    const lastUpdated = user.lastUpdated;
+    const copyUser = { ...user, lastUpdated };
+    return { faunaUser: copyUser, faunaTokens: tokens };
   } catch (error) {
     console.error(error);
     return undefined;
