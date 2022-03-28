515c8806f1fa9afedaf5e99fa0a4ff2eaa89a319
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-28 21:28:41 +0800

    feat: add the new responsive navigation bar

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,15 +1,16 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { AuthContextProvider } from '../stores/netlifyIdentityContext';
-import Nav from '../components/Nav';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import NavigationBar from '../components/NavBar/NavBar';
+import { AuthContextProvider } from '../stores/netlifyIdentityContext';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <CSSReset />
       <AuthContextProvider>
-        <Nav />
+        {/* <Nav /> */}
+        <NavigationBar />
         <Component {...pageProps} />
       </AuthContextProvider>
     </ChakraProvider>
