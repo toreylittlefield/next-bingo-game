b42421cad4921881ef66f5f8e9a2f40096f492c6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-03-28 21:27:22 +0800

    feat: component for navigation items in navbar

---
+++ client/components/NavBar/NavItem.tsx
@@ -0,0 +1,48 @@
+import { Flex, FlexProps, Icon, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { ReactText } from 'react';
+import { IconType } from 'react-icons';
+
+interface NavItemProps extends FlexProps {
+  icon: IconType;
+  href: string;
+  children: ReactText;
+}
+const NavItem = ({ icon, href, children, ...rest }: NavItemProps) => {
+  return (
+    <NextLink href={href} passHref>
+      <Link style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
+        <Flex
+          minWidth={112}
+          align="center"
+          p="4"
+          mx="4"
+          borderRadius="lg"
+          role="group"
+          cursor="pointer"
+          _hover={{
+            bg: 'whiteAlpha.300',
+            color: { base: '#831F3B', md: 'whiteAlpha.900' },
+          }}
+          color={{ base: 'blackAlpha.900', md: 'whiteAlpha.800' }}
+          {...rest}
+        >
+          {icon && (
+            <Icon
+              mr="4"
+              fontSize={{ base: 20, lg: 18, xl: 24 }}
+              _groupHover={{
+                color: { base: '#831F3B', md: 'whiteAlpha.900' },
+              }}
+              as={icon}
+            />
+          )}
+          {children}
+        </Flex>
+      </Link>
+    </NextLink>
+  );
+};
+
+export { NavItem };
+
